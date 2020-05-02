d446c892ed1b2f2a93ed28b0ca959638f1d16e4e
Federico Jimenez <fedeejimenez@gmail.com>
2020-05-02 20:59:42 +0100

    Fix issue when getting data from api

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -5,7 +5,7 @@ class CurrenciesController < ApplicationController
 
   def search
     @currencies = Currency.where('LOWER(name) LIKE ?',
-                                 "%#{params[:search].downcase}")
+                                 "%#{params[:search].downcase}%")
 
     render json: { currencies: @currencies }
   end
